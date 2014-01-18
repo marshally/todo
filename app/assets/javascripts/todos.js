41fb17b8dd0afe6ebee824b113c30aede6347d96
Marshall Yount <marshall@yountlabs.com>
2014-01-18 10:30:08 -0600

    submit todos create, update and destroy via ajax

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,6 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready(function() {
   $(".datepicker").each(function() {
     $(this).datepicker({
@@ -8,4 +5,55 @@ $(document).ready(function() {
       dateFormat: "yy-mm-dd"
     });
   });
+  $("input[type='submit']").each(function() {
+    $(this).click(function(e) {
+      button = e.currentTarget;
+      form = button.form;
+
+      if (button.value == "Create") {
+        create_todo(form.id);
+      }
+      else if (button.value == "Save") {
+        update_todo(form.id);
+      }
+      else if (button.value == "Delete") {
+        delete_todo(form.id);
+      }
+      else {
+
+      }
+    });
+  });
 });
+
+function create_todo(form_id) {
+  $.create(
+    '/todos',
+    $("#"+form_id).serialize(),
+    function (reponse) {
+      alert('successfully added todo.');
+    }
+  );
+}
+
+function update_todo(form_id) {
+  todo_id = form_id.match(/\d+$/);
+
+  $.update(
+    '/todos/'+todo_id,
+    $("#"+form_id).serialize(),
+    function (reponse) {
+      alert('successfully updated todo.');
+    }
+  );
+}
+
+function delete_todo(form_id) {
+  todo_id = form_id.match(/\d+$/);
+  $.destroy({
+    url: '/todos/'+todo_id,
+    success: function (response) {
+      alert('successfully deleted task.');
+    }
+  });
+}
