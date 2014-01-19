25c852b8799f334ac01f2ba6dc7eff4f06cab717
Marshall Yount <marshall@yountlabs.com>
2014-01-19 11:18:37 -0600

    rake db:migrate (tokens)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119163007) do
+ActiveRecord::Schema.define(version: 20140119171712) do
 
   create_table "todos", force: true do |t|
     t.string   "description",                 null: false
@@ -38,8 +38,10 @@ ActiveRecord::Schema.define(version: 20140119163007) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
