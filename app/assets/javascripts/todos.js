7b4e81104284b0bf06d2a1d483470beb91124c26
Marshall Yount <marshall@yountlabs.com>
2014-01-19 14:05:33 -0600

    cleanup javascript

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -42,60 +42,73 @@ function handle_click(e) {
 }
 
 function create_todo(form_id) {
+  form = $("#new_todo");
   $.create({
     url: '/todos',
-    data: $("#"+form_id).serialize(),
-    error: function(response) {
-      $("#new_todo>div.error").html(response.responseText);
-      $("#new_todo").effect("highlight", {color: 'red'}, 3000);
-    },
+    data: form.serialize(),
     success: function (response) {
+      clear_error(form);
+
       created = $("#new_todo_0").clone();
       id = "edit_todo_"+response.id;
       created.attr("id", id);
-      $("#new_todo>div>input").each(function(e) {
+      form.find("div>input").each(function(e) {
         if (this.name.substring(0, 4) === "todo") {
           created.find("input[name='"+this.name+"']")[0].value = this.value;
           this.value = "";
         }
       });
-      created.insertBefore("#new_todo");
+      created.insertAfter("#new_todo");
       created.effect("highlight", 1000);
       todo_buttons().on("click", handle_click);
-      $("#new_todo>div.error").html("");
       sort(window.order_by);
+    },
+    error: function(response) {
+      set_error(form, response.responseText);
+      form.effect("highlight", {color: 'red'}, 3000);
     }
   });
 }
 
 function update_todo(form_id) {
   todo_id = form_id.match(/\d+$/);
+  form = $("#"+form_id);
 
   $.update({
     url: '/todos/'+todo_id,
-    data: $("#"+form_id).serialize(),
+    data: form.serialize(),
     success: function (response) {
-      $("#"+form_id+">div.error").html("");
-      $("#"+form_id).effect("highlight", 1000);
+      clear_error(form);
+      form.effect("highlight", 1000);
       sort(window.order_by);
       // there is a bug in sorting where if you update a record it doesn't
       // ever sort to the right place.
     },
     error: function (response) {
-      $("#"+form_id+">div.error").html(response.responseText);
-      $("#"+form_id).effect("highlight", {color: 'red'}, 3000);
+      set_error(form, response.responseText);
+      form.effect("highlight", {color: 'red'}, 3000);
     }
   });
 }
 
 function delete_todo(form_id) {
   todo_id = form_id.match(/\d+$/);
+  form = $("#"+form_id);
+
   $.destroy({
     url: '/todos/'+todo_id,
     success: function (response) {
-      $("#"+form_id).remove();
+      form.remove();
     }
   });
+}
+
+function set_error(form, value) {
+  form.find(">div.error").html(value);
+}
+
+function clear_error(id) {
+  set_error(id, "");
 }
 
 function sort(field) {
