98ba835c3ae30b4e1b65f964e908da603bbac302
Marshall Yount <marshall@yountlabs.com>
2014-01-19 14:41:35 -0600

    todo belongs_to user

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -12,6 +12,8 @@
 #
 
 class Todo < ActiveRecord::Base
+  belongs_to :user
+
   validates :description, presence: true
   validates :priority, numericality: true
 
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -14,6 +14,8 @@
 require 'spec_helper'
 
 describe Todo do
+  it { should belong_to(:user) }
+
   it { should validate_presence_of(:description) }
   it { should validate_numericality_of(:priority) }
 
